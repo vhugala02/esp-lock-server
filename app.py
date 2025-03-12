b45c8ab1815a592141d2f288cc231ba16a3aaa44
vhugala02 <vhugalaraps@gmail.com>
2025-03-12 10:43:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,4 @@
-from flask import Flask, request, jsonify
+from main import app
 
-app = Flask(__name__)
-
-@app.route('/bike_selection', methods=['POST'])
-def bike_selection():
-    try:
-        data = request.get_json()
-        bike = data.get("bike")  # Extract selected bike
-        if bike:
-            print(f"Received bike selection: {bike}")
-            return jsonify({"status": "success", "message": f"Bike {bike} selected"}), 200
-        else:
-            return jsonify({"status": "error", "message": "No bike selected"}), 400
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
